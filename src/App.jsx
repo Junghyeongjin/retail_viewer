b516b1ebf45ca4e27b9e4f9d57fe6c6156e5ed6e
정형진(Jung Hyeong-jin) <hjjung@hansae.com>
2026-03-05 19:04:43 +0900

    fix render

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import RetailPriceBenchmark from "./retail_price_benchmark_v3";
+import RetailViewer from "./retail_price_benchmark_v3";
 
 function App() {
-  return <RetailPriceBenchmark />;
+  return <RetailViewer />;
 }
 
 export default App;
